d9dec3ddda0b789e80f71961609ec996163d9ba9
razibdash <avrorazib@gmail.com>
2025-07-11 00:42:32 +0600

    ceate root route and render the html file,and also create another routes for upload files

--- app.py
+++ app.py
@@ -9,3 +9,31 @@ import aiofiles
 import json
 import csv
 from src.helper import llm_pipeline
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+@app.post("/upload")
+async def chat(request: Request, pdf_file: bytes = File(), filename: str = Form(...)):
+    base_folder = 'static/docs/'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    pdf_filename = os.path.join(base_folder, filename)
+
+    async with aiofiles.open(pdf_filename, 'wb') as f:
+        await f.write(pdf_file)
+ 
+    response_data = jsonable_encoder(json.dumps({"msg": 'success',"pdf_filename": pdf_filename}))
+    res = Response(response_data)
+    return res
+
+if __name__ == "__main__":
+  uvicorn.run("app:app", host="localhost", port=8000,reload=True,)
--- src/helper.py
+++ src/helper.py
@@ -1,12 +1,12 @@
 import os
 from dotenv import load_dotenv
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_groq import ChatGroq
 from langchain.text_splitter import TokenTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.docstore.document import Document
 from src.prompt import *
